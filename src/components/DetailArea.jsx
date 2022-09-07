2c7389a840c1fc2b9d43351a6c632e52bed8dba9
momo <y.mmk.0508@gmail.com>
2022-09-07 01:05:09 +0000

    detailAreaのoverlay設定

--- src/components/DetailArea.jsx
+++ src/components/DetailArea.jsx
@@ -7,16 +7,18 @@ const DetailArea = (props) => {
     
     if (showLyric){
         return (
-            <div className="detailArea">
-                {songs.map((song) => {
-                    return (
-                        <>
-                            <h2 className="title" key={song[0]}>{song[0]}</h2>
-                            <div className="lyric">{song[2]}</div>
-                        </>
-                    )
-                })}
-                <button className="closeButton" onClick={onClickLyricClose}>close</button>
+            <div className="overlay">
+                <div className="detailArea">
+                    {songs.map((song) => {
+                        return (
+                            <>
+                                <h2 className="title" key={song[0]}>{song[0]}</h2>
+                                <div className="lyric">{song[2]}</div>
+                            </>
+                        )
+                    })}
+                    <button className="closeButton" onClick={onClickLyricClose}>close</button>
+                </div>
             </div>
         )
     }
